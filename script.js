87733e4a453313be99821984b804abdff11f8b25
LJ Spencer <hockeyduck3@gmail.com>
2020-03-14 20:34:08 -0600

    Added var's for length, lower, upper, and special.

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  
+  var length = prompt('How long would you like your password to be?');
+
+  var lower = confirm('Would you like the password to have lower case letters?');
+
+  var upper = confirm('Would you like the password to have upper case letters?');
+
+  var special = confirm('Would you like the password to include special characters?');
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
