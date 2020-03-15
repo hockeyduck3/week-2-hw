8945aefd1c964deefef70252a8aabb23f5cd0bbc
LJ Spencer <hockeyduck3@gmail.com>
2020-03-15 13:43:00 -0600

    Added to the first if statement, so if the length is blank. Also made it so after the function checks for null, blank, and if it's less than 8 or more than 128, it will make the string into a number, then check and see if it's NaN. Finally, took out the + 1 in the function passwordMaker.

--- script.js
+++ script.js
@@ -8,40 +8,49 @@ function writePassword() {
   
   var length = prompt('How long would you like your password to be?');
 
-  if (length == null) {
+  if (length == null || length === '') {
     return;
   } else if (length < 8 || length > 128) {
     alert('Please keep the password between 8 and 128 characters.');
     writePassword();
   } else {
-    var upper = confirm('Would you like the password to have upper case letters?');
+    length = parseInt(length);
 
-    var special = confirm('Would you like the password to include special characters?');
-
-    var numbers = confirm('Finally, would you like the password to include numbers?');
-
-    var upperSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-    var lowerSet = "abcdefghijklmnopqrstuvwxyz";
-
-    var specialSet = `!"#$%&'()*+,-./:;<=>?@[\]^_{|}~`;
-
-    var numbersSet = '1234567890';
-
-    if (upper === true) {
-      lowerSet = lowerSet.concat(upperSet);
-    } if (special === true) {
-      lowerSet = lowerSet.concat(specialSet);
-    } if (numbers === true ) {
-      lowerSet = lowerSet.concat(numbersSet);
-    }
-
-    function passwordMaker() {
-      for (var i = 0; i < length; i++) {
-        password += (lowerSet.charAt(Math.floor(Math.random() * lowerSet.length) + 1));
+    if (isNaN(length)){
+      alert("Please make sure you're typing in a number.")
+      writePassword();
+    } else {
+        var upper = confirm('Would you like the password to have upper case letters?');
+    
+        var special = confirm('Would you like the password to include special characters?');
+    
+        var numbers = confirm('Finally, would you like the password to include numbers?');
+    
+        var upperSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    
+        var lowerSet = "abcdefghijklmnopqrstuvwxyz";
+    
+        var specialSet = `!"#$%&'()*+,-./:;<=>?@[\]^_{|}~`;
+    
+        var numbersSet = '1234567890';
+    
+        if (upper === true) {
+          lowerSet = lowerSet.concat(upperSet);
+        } if (special === true) {
+          lowerSet = lowerSet.concat(specialSet);
+        } if (numbers === true ) {
+          lowerSet = lowerSet.concat(numbersSet);
+        }
+    
+        function passwordMaker() {
+          for (var i = 0; i < length; i++) {
+            password += (lowerSet.charAt(Math.floor(Math.random() * lowerSet.length)));
+          }
+        }
       }
     }
-  }
+
+
   passwordMaker();
 
   // var password = generatePassword();
