f72d9ec763c6561bfa0ff8ac615fdc8742dc22fe
LJ Spencer <hockeyduck3@gmail.com>
2020-03-15 01:13:00 -0600

    Added new var's and added a function passwordMaker()

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
+  var password = '';
   
   var length = prompt('How long would you like your password to be?');
 
@@ -12,7 +14,34 @@ function writePassword() {
 
   var special = confirm('Would you like the password to include special characters?');
 
-  var password = generatePassword();
+  var numbers = confirm('Finally, would you like the password to include numbers?');
+
+  var upperSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  var lowerSet = "abcdefghijklmnopqrstuvwxyz";
+
+  var specialSet = `!"#$%&'()*+,-./:;<=>?@[\]^_{|}~`;
+
+  function passwordMaker() {
+    console.log(length);
+    for (var i = 0; i < length; i++) {
+      if (lower === true) {
+        password += (lowerSet.charAt(Math.floor(Math.random() * length)))
+      }
+      if (upper === true) {
+        password += (upperSet.charAt(Math.floor(Math.random() * length)))
+      }
+      if (special === true) {
+        password += (specialSet.charAt(Math.floor(Math.random() * length)))
+      }
+      if (numbers === true) {
+        password += (Math.floor(Math.random() * length))
+      }
+    }
+  }
+  passwordMaker();
+  console.log(password);
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
